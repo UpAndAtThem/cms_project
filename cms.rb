88c46ce5e2a5c234825c20d85eb2151478dde14d
Jason Nelson <Jasonn318@gmail.com>
2019-03-15 22:22:08 -0500

    add /:file_name route to serve files from system drive

--- cms.rb
+++ cms.rb
@@ -5,9 +5,20 @@ require "tilt/erubis"
 require 'pry'
 require "find"
 
+before do
+  @files_directory = Find::find("./public/data")
+  @file_names = Dir.foreach("./public/data").select { |x| File.file?("./public/data/" + x)}.sort
+end
+
 get "/" do
-  @files_directory = Find::find("./public/files")
-  @file_names = Dir.foreach("./public/files").select { |x| File.file?("./public/files/" + x)}.sort
-  
   erb :files
+end
+
+get "/:file_name" do
+  @file_name = params['file_name']
+
+  #headers["Content-Type"] = "text/plain"
+  @file = File.read("./public/data/#{@file_name}")
+
+  erb :file
 end
